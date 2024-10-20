6a7ab53212ab5e55b24ed6bb6d36825fd2d1760e
Mika Steyer <mikasteyer9@gmail.com>
2024-10-20 13:34:53 +0200

    feat: add logs to prisma service

--- src/services/prisma/prisma.service.ts
+++ src/services/prisma/prisma.service.ts
@@ -1,13 +1,31 @@
-import { Injectable, type OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 import { prismaSoftDeleteMiddleware } from './middleware/soft-delete';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     this.$use(prismaSoftDeleteMiddleware);
 
+    Logger.log('PrismaService initialized', 'PrismaService');
+
     await this.$connect();
+
+    Logger.log('PrismaClient connected', 'PrismaService');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+
+    Logger.log('PrismaClient disconnected', 'PrismaService');
   }
 }
